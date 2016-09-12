ca0027154c0386e2d85c41a8041ec10c86fc77f7
Johan Brichau <johan@yesplan.be>
2016-09-12 15:36:04 +0200

    various experiments with stepper control

--- door-actuator.ino
+++ door-actuator.ino
@@ -1,9 +1,13 @@
+#include "AccelStepperSpark.h"
+
 #define MOTOR_DIR_PIN D0
 #define MOTOR_STEP_PIN D1
 #define MOTOR_ENABLE_PIN D2
 #define MOTOR_SLEEP_PIN D3
 #define END_OF_TRAVEL_LEFT_PIN D4
 #define END_OF_TRAVEL_RIGHT_PIN D5
+
+AccelStepper stepper(AccelStepper::DRIVER, MOTOR_STEP_PIN, MOTOR_DIR_PIN);
 
 void setup() {
   pinMode(MOTOR_DIR_PIN,OUTPUT);
@@ -25,21 +29,68 @@ void loop() {
 }
 
 void closeDoor() {
-  digitalWrite(MOTOR_DIR_PIN, HIGH);
+  //digitalWrite(MOTOR_DIR_PIN, HIGH);
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(20);
+  stepper.moveTo(-200);
   moveToEnd(END_OF_TRAVEL_LEFT_PIN);
 }
 
 void openDoor() {
-  digitalWrite(MOTOR_DIR_PIN, LOW);
+  //digitalWrite(MOTOR_DIR_PIN, LOW);
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(20);
+  stepper.moveTo(200);
   moveToEnd(END_OF_TRAVEL_RIGHT_PIN);
 }
 
+void moveToEnd(int end_of_travel_pin) {
+  int end_of_travel = digitalRead(end_of_travel_pin);
+  while(!end_of_travel) {
+    if (stepper.distanceToGo() == 0)
+      stepper.moveTo(-stepper.currentPosition());
+    stepper.run();
+    end_of_travel = digitalRead(end_of_travel_pin);
+  }
+}
+
+void motorSleep() {
+  digitalWrite(MOTOR_SLEEP_PIN,LOW);
+}
+
+void motorWake() {
+  digitalWrite(MOTOR_SLEEP_PIN,HIGH);
+}
+
+// This was code to do the timings manually
+void moveToEnd_DIRECT(int end_of_travel_pin) {
+  motorWake();
+  int end_of_travel = digitalRead(end_of_travel_pin);
+  int i = 50000;
+  int steps = 0;
+  while(!end_of_travel) {
+    digitalWrite(MOTOR_STEP_PIN,HIGH);
+    //delayMicroseconds(100);
+    digitalWrite(MOTOR_STEP_PIN,LOW);
+    delayMicroseconds(i);
+    steps++;
+    if(i>1600) {
+      if(steps%50 == 0) i = i/2;
+    }
+    else
+      i = 1000;
+    end_of_travel = digitalRead(end_of_travel_pin);
+  };
+  motorSleep();
+}
+
+// This was an experiment to see how fast we can make it run
 int slow = 400;
-int fast = 1400;
+int fast = 1200;
 int accelSteps = 100;
 int freqStep = (fast - slow) / accelSteps;
 
-void moveToEnd(int end_of_travel_pin) {
+void moveToEnd_PWM(int end_of_travel_pin) {
   int end_of_travel = digitalRead(end_of_travel_pin);
   motorWake();
   for(int i=0;i<=accelSteps;i++) {
@@ -57,25 +108,4 @@ void moveToEnd(int end_of_travel_pin) {
   };
   analogWrite(MOTOR_STEP_PIN,0,1000);
   motorSleep();
-}
-
-void moveToEnd2(int end_of_travel_pin) {
-  motorWake();
-  int end_of_travel = digitalRead(end_of_travel_pin);
-  while(!end_of_travel) {
-    digitalWrite(MOTOR_STEP_PIN,HIGH);
-    delay(1);
-    digitalWrite(MOTOR_STEP_PIN,LOW);
-    //delay(1);
-    end_of_travel = digitalRead(end_of_travel_pin);
-  };
-  motorSleep();
-}
-
-void motorSleep() {
-  digitalWrite(MOTOR_SLEEP_PIN,LOW);
-}
-
-void motorWake() {
-  digitalWrite(MOTOR_SLEEP_PIN,HIGH);
 }
